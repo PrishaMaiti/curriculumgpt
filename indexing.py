445d07fae8d7c895723b957e9dec62dd104af058
Prisha Maiti <89211997+PrishaMaiti@users.noreply.github.com>
2023-06-24 12:25:06 -0700

    indexing.py works now

--- indexing.py
+++ indexing.py
@@ -8,27 +8,28 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+# os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Set persist directory
 persist_directory = 'db'
 
-buffett_loader = DirectoryLoader('./docs/buffett/', glob="*.pdf")
-branson_loader = DirectoryLoader('./docs/branson/', glob="*.pdf")
+business_loader = DirectoryLoader('./docs/business/', glob="*.pdf")
+engr_loader = DirectoryLoader('./docs/engr/', glob="*.pdf")
 
-buffett_docs = buffett_loader.load()
-branson_docs = branson_loader.load()
+business_docs = business_loader.load()
+engr_docs = engr_loader.load()
 
 embeddings = OpenAIEmbeddings()
 text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=8)
 
 # Split documents and generate embeddings
-buffett_docs_split = text_splitter.split_documents(buffett_docs)
-branson_docs_split = text_splitter.split_documents(branson_docs)
+business_docs_split = text_splitter.split_documents(business_docs)
+engr_docs_split = text_splitter.split_documents(engr_docs)
 
 # Create Chroma instances and persist embeddings
-buffettDB = Chroma.from_documents(buffett_docs_split, embeddings, persist_directory=os.path.join(persist_directory, 'buffett'))
-buffettDB.persist()
+business_docs = Chroma.from_documents(business_docs_split, embeddings, persist_directory=os.path.join(persist_directory, 'business'))
+business_docs.persist()
 
-bransonDB = Chroma.from_documents(branson_docs_split, embeddings, persist_directory=os.path.join(persist_directory, 'branson'))
-bransonDB.persist()
+engr_docs = Chroma.from_documents(engr_docs_split, embeddings, persist_directory=os.path.join(persist_directory, 'engr'))
+engr_docs.persist()
